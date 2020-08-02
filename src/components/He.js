4320d2f0f2bb4241e55abef641e55185acdc80d1
viharika123 <viharikabhavanasi@gmail.com>
2020-08-02 11:45:58 -0500

    deploy

---
+++ src/components/He.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import './Style.css'
+const He = () => {
+    return(
+        <div className="b"> 
+              <h1 className="h">Mental Health</h1>
+              <p className = "tag">
+              Mental health refers to our emotional wellbeing, it is all about how we think, feel and behave.
+              The relevance of working with patients with mental health problems for me will be a challenge to start with.
+              While on my placement I come across many patients why are suffering with Alzheimer and Dementia and most are suffering with mental health disorders of various kinds.
+              In my central discussion I intend to cover the case of one patient named Joe who has metal health problems.
+              Mental health is a term that encompasses a range of experiences and situations. 
+              It can be an on going experience from mental wellbeing through to a severe and enduring mental illness affecting a person’s overall emotional and psychological condition. 
+              Incidents in life such as bereavement, financial and personal happiness such as the way we feel about ourselves can lead to depression and anxiety.
+              </p>
+              <div className = "row">
+              <div className="col-md-6 ">
+              <img src={"/images/health-is-wealth1.jpeg"}  className = "i"alt ="" /></div>
+               <div className="col-md-6"> <img src={"/images/Greatest-wealth-is-health.png"}  className = "i"alt ="" /> </div>
+              </div>
+              <div className="tag">
+              Mental health refers to our emotional wellbeing, it is all about how we think, feel and behave.
+              The relevance of working with patients with mental health problems for me will be a challenge to start with.
+              While on my placement I come across many patients why are suffering with Alzheimer and Dementia and most are suffering with mental health disorders of various kinds.
+              In my central discussion I intend to cover the case of one patient named Joe who has metal health problems.
+              Mental health is a term that encompasses a range of experiences and situations. 
+              It can be an on going experience from mental wellbeing through to a severe and enduring mental illness affecting a person’s overall emotional and psychological condition. 
+              Incidents in life such as bereavement, financial and personal happiness such as the way we feel about ourselves can lead to depression and anxiety.
+              </div>
+          </div>
+    )
+}
+export default He
+
+
